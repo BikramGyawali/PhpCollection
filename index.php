d236b63e2647d326079f577aecaabfb1d9eb1e44
BikramGyawali <gyawalibikram0@gmail.com>
2025-07-10 13:14:10 +0545

    Done server side validation

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <?php
 include("connection.php");
 if(isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $phone=$_POST['phone'];
-    $gender=$_POST['gender'];
-    $country=$_POST['country'];
+    
+    //for validation
 
-    $query="INSERT  INTO pdetail(name,email,phone,gender,country) VALUES('$name','$email','$phone','$gender','$country')";
+    function valid($data){
+        $data=trim($data);
+        $data=stripslashes($data);
+        $data=htmlspecialchars($data);
+        return $data;
+    }
+    $name=valid($_POST['name']);
+    $email=valid($_POST['email']);
+    $phone=valid($_POST['phone']);
+    $gender=valid($_POST['gender']);
+    $country=valid($_POST['country']);
+
+    $errors=[];
+// check for all field
+    if(empty($name)||empty($email)||empty($phone)||empty($gender)||empty($country)){
+        $errors[]="All fields are require";
+    }
+// for phone number pattern
+
+if(!preg_match("/^[0-9]{10}$/",$phone)){
+    $errors[]="Doesnt phone number match the pattern";
+}
+//for email
+if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+    $errors[]="Doesnot match the email pattern";
+}
+if(!empty($errors)){
+    foreach($errors as $error){
+        echo "<p style='color:red;'>* $error</p>";
+    }
+}
+else{
+ $query="INSERT  INTO pdetail(name,email,phone,gender,country) VALUES('$name','$email','$phone','$gender','$country')";
 $data=mysqli_query($conn,$query);
     if($data){
         echo"Data inserted sucessfully";
@@ -16,6 +45,9 @@ $data=mysqli_query($conn,$query);
         echo"Error in insertion";
     }
     mysqli_close($conn);
+}
+   
+
 }
 ?>
 
@@ -71,24 +103,24 @@ $data=mysqli_query($conn,$query);
    <div class="section">
     <div class="form-group">
         <label for="name">Name:</label>
-    <input type="text" name="name" id="name" required>
+    <input type="text" name="name" id="name" >
     </div>
    <div class="form-group"> <label for="email">
         Email:
     </label>
-    <input type="email" name="email" id="email" required></div>
+    <input type="email" name="email" id="email" ></div>
 <div class="form-group">
     <label for="phone">Phone:</label>
-    <input type="text" name="phone" id="phone" required></div>
+    <input type="text" name="phone" id="phone" ></div>
 
  <div class="form-group">
        <label for="gender">Gender:</label>
-    <input type="text" name="gender" id="gemder" required>
+    <input type="text" name="gender" id="gender" >
  </div>
 
    <div class="form-group">
      <label for="country">Country:</label>
-    <input type="text" name="country" id="country" required>
+    <input type="text" name="country" id="country" >
    </div>
    <button type="submit"name="submit">Submit</button>
    </div>
